5f08ef392c63607247f6293bd02125bce4acc35b
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-11-07 19:00:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pengelolahalaman/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.pengelolahalaman.data
+
+import com.example.pengelolahalaman.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche,
+    )
+}
