41a20e65ed4e9059ac659580dc247272f29a82e8
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-11-13 22:28:15 +0700

    Membuat variable di HalamanSatu

--- app/src/main/java/com/example/pengelolahalaman/HalamanSatu.kt
+++ app/src/main/java/com/example/pengelolahalaman/HalamanSatu.kt
@@ -1,10 +1,19 @@
 package com.example.pengelolahalaman
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 
 @Composable
 fun HalamanSatu(
     onSubmitButtonClicked : (MutableList<String>) -> Unit
 ){
+    var nama by rememberSaveable { mutableStateOf(value = "") }
+    var noHp by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var listData: MutableList<String> = mutableListOf(nama, noHp, alamat)
 
 }
